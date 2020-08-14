6213b17594e305ac64edc6833b1cc3d7a6880124
Ashish Kularia <ashishkularia@gmail.com>
2020-08-14 20:11:53 +1000

    all assessment completed

---
+++ project/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','indiana.edu','berkeley.edu','mac.com','cam.ac.uk','ucdavis.edu','gmail.com','uct.ac.za','ufp.pt','virginia.edu'],
+['2005',57,12,12,12,7,11,10,14,10,9],
+['2006',295,94,86,79,78,74,72,59,55,52]
+];
